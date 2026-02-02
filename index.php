bfdf63c4959dbbc16e01efb04df077609d0af0e2
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-02 16:15:18 +0600

    adding basic files, folders and links to this projects

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Makfare Project</title>
+
+    <!-- fav icon link  -->
+    <link rel="shortcut icon" href="./assets/makfare_logo_new.png" type="image/x-icon">
+
+    <!-- daisyui cdn link  -->
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
+    <!-- tailwind css cdn link -->
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+</head>
+
+<body>
+    <h class="text-xl text-red-500 btn">MarkFare</h>
+</body>
+
+</html>
