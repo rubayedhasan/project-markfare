46f1c46d49339d4debae7743014d69b76ae7c4f1
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-08 12:09:12 +0600

    adding navbar section's responsive styles for all devices

--- index.php
+++ index.php
@@ -103,17 +103,23 @@
         </section>
 
         <!-- section::navbar  -->
-        <nav class="navbar bg-[#054F731A] border border-[#054F731A] rounded-md shadow-sm mt-8 xl:mt-0">
+        <nav class="navbar
+         bg-[#054F731A]
+         backdrop-blur-md
+         border border-white/10
+         rounded-xl
+         shadow-[0_5px_15px_rgba(0,0,0,0.25)]
+         mt-8 xl:mt-0">
             <div class="navbar-start lg:hidden">
                 <div class="dropdown">
-                    <div tabindex="0" role="button" class="border border-[#551a8b]/60 bg-[#551a8b]/40 p-2 rounded-md lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 md:h-6 w-4 md:w-6" fill="#551a8b" viewBox="0 0 24 24" stroke="currentColor">
+                    <div tabindex="0" role="button" class="border border-[#551a8b]/60 p-2 rounded-md lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 md:h-6 w-5 md:w-6" fill="#551a8b" viewBox="0 0 24 24" stroke="#551a8b">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
                         </svg>
                     </div>
                     <ul
                         tabindex="-1"
-                        class="menu menu-sm dropdown-content bg-base-100 border border-[#551a8b]/60 rounded-box z-1 w-52 p-2 shadow mt-4">
+                        class="menu menu-sm dropdown-content bg-base-100 border border-[#551a8b]/60 rounded-box z-1 w-52 p-2 shadow mt-5">
                         <li>
                             <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Home</a>
                         </li>
@@ -157,7 +163,7 @@
                 <!-- button container section  -->
                 <div class="cta-button-container inline-flex gap-2 md:gap-4">
                     <!-- btn:contact us  -->
-                    <button class="font-inter font-medium text-xs md:text-sm px-2.5 md:px-5 py-1 md:py-2.5 rounded-lg text-[#551a8b] border border-[#551a8b]/60 transition-all duration-200 hover:text-white hover:bg-gradient-to-br hover:from-[#551a8b] hover:to-[#054f73] hover:shadow-[0_4px_14px_rgba(85,26,139,0.35)] focus:outline-none focus:ring-2 focus:ring-[#551a8b]/40" type="button">Contact Us</button>
+                    <button class="font-inter font-medium text-xs md:text-sm px-2.5 sm:px-3 md:px-5 py-1 sm:py-2 md:py-2.5 rounded-lg text-[#551a8b] border border-[#551a8b]/60 transition-all duration-200 hover:text-white hover:bg-gradient-to-br hover:from-[#551a8b] hover:to-[#054f73] hover:shadow-[0_4px_14px_rgba(85,26,139,0.35)] focus:outline-none focus:ring-2 focus:ring-[#551a8b]/40" type="button">Contact Us</button>
 
                     <!-- btn:get storage  -->
                     <button class="font inter font-medium text-xs md:text-sm px-2.5 md:px-5 py-1 md:py-2.5 rounded-lg text-[#551a8b] border border-[#551a8b]/60 transition-all duration-200 hover:text-white hover:bg-gradient-to-br hover:from-[#551a8b] hover:to-[#054f73] hover:shadow-[0_4px_14px_rgba(85,26,139,0.35)] focus:outline-none focus:ring-2 focus:ring-[#551a8b]/40" type="button">Get Storage</button>
