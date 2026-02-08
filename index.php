b98d7850fd2bca1677b72372021535256a0c2e28
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-08 17:35:15 +0600

    build the basic structue of the carousel or slider section with hero or banner

--- index.php
+++ index.php
@@ -27,7 +27,67 @@
 
     <!-- section:: main  -->
     <main class="w-4/5 mx-auto my-10 md:my-14 rounded-md">
-        <p>Up Coming Page</p>
+        <!-- section:: banner  -->
+        <section id="banner" class="">
+            <div class="carousel w-full h-[600px] rounded-lg">
+                <div id="item1" class="carousel-item w-full">
+                    <div
+                        class="hero h-full"
+                        style="background-image: url(./assets/cold_storage/cold_storage.jpg);">
+                        <div class="hero-overlay"></div>
+                        <div class="hero-content text-neutral-content text-center">
+                            <div class="max-w-md">
+                                <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p class="mb-5">
+                                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                </p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="item2" class="carousel-item w-full">
+                    <div
+                        class="hero h-full"
+                        style="background-image: url(./assets/cold_storage/cold_storage_wideview.jpg);">
+                        <div class="hero-overlay"></div>
+                        <div class="hero-content text-neutral-content text-center">
+                            <div class="max-w-md">
+                                <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p class="mb-5">
+                                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                </p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div id="item3" class="carousel-item w-full">
+                    <div
+                        class="hero h-full"
+                        style="background-image: url(./assets/cold_storage/bages_of_goods.jpg);">
+                        <div class="hero-overlay"></div>
+                        <div class="hero-content text-neutral-content text-center">
+                            <div class="max-w-md">
+                                <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+                                <p class="mb-5">
+                                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                                </p>
+                                <button class="btn btn-primary">Get Started</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="flex w-full justify-center gap-2 py-2">
+                <a href="#item1" class="btn btn-xs">1</a>
+                <a href="#item2" class="btn btn-xs">2</a>
+                <a href="#item3" class="btn btn-xs">3</a>
+            </div>
+        </section>
     </main>
 
     <!-- section:: footer  -->
