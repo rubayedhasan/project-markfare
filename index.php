bad90d19f7c73fb838fd79408deafcca3e35bd4a
Md. Rubayed Hasan <mdrubayedhasan2002@gmail.com>
2026-02-08 11:38:08 +0600

    fixing navbar menu's style

--- index.php
+++ index.php
@@ -103,34 +103,31 @@
         </section>
 
         <!-- section::navbar  -->
-        <nav class="navbar bg-[#054F731A] shadow-sm mt-8 xl:mt-0">
+        <nav class="navbar bg-[#054F731A] border border-[#054F731A] rounded-md shadow-sm mt-8 xl:mt-0">
             <div class="navbar-start lg:hidden">
                 <div class="dropdown">
-                    <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+                    <div tabindex="0" role="button" class="btn btn-ghost border border-[#551a8b]/60 lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" />
                         </svg>
                     </div>
                     <ul
                         tabindex="-1"
-                        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        class="menu menu-sm dropdown-content bg-base-100 border border-[#551a8b]/60 rounded-box z-1 w-52 p-2 shadow mt-[16px]">
                         <li>
-                            <a class="font-bold hover:font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Home</a>
+                            <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Home</a>
                         </li>
                         <li>
-                            <a class="font-bold hover:font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Our Work</a>
+                            <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Our Work</a>
                         </li>
                         <li>
-                            <a class="font-bold hover:font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Tissue Culture</a>
+                            <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Tissue Culture</a>
                         </li>
                         <li>
-                            <a class="font-bold hover:font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Blog</a>
+                            <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Blog</a>
                         </li>
                         <li>
-                            <a class="font-bold hover:font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">About</a>
-                        </li>
-                        <li>
-                            <a class="font-bold hover:font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Contact</a>
+                            <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">About</a>
                         </li>
                     </ul>
                 </div>
@@ -138,27 +135,33 @@
             <div class="navbar-start hidden lg:flex">
                 <ul class="menu menu-horizontal px-1">
                     <li>
-                        <a class="font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Home</a>
+                        <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Home</a>
                     </li>
                     <li>
-                        <a class="font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Our Work</a>
+                        <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Our Work</a>
                     </li>
                     <li>
-                        <a class="font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Tissue Culture</a>
+                        <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Tissue Culture</a>
                     </li>
                     <li>
-                        <a class="font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Blog</a>
+                        <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">Blog</a>
                     </li>
                     <li>
-                        <a class="font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">About</a>
-                    </li>
-                    <li>
-                        <a class="font-extrabold hover:text-[#551A8B] hover:bg-transparent" href="">Contact</a>
+                        <a class="font-semibold text-sm text-opacity-80 relative after:absolute after:left-0 after:-bottom-1 after:h-[1.5px] after:w-0 after:bg-[#551a8b] after:transition-all after:duration-300 hover:text-[#551a8b] hover:bg-transparent hover:after:w-full" href="">About</a>
                     </li>
                 </ul>
             </div>
+
+            <!-- section:: CTA button  -->
             <div class="navbar-end">
-                <a class="btn">Button</a>
+                <!-- button container section  -->
+                <div class="cta-button-container inline-flex gap-4">
+                    <!-- btn:contact us  -->
+                    <button class="font-inter font-medium text-sm px-5 py-2.5 rounded-lg text-[#551a8b] border border-[#551a8b]/60 transition-all duration-200 hover:text-white hover:bg-gradient-to-br hover:from-[#551a8b] hover:to-[#054f73] hover:shadow-[0_4px_14px_rgba(85,26,139,0.35)] focus:outline-none focus:ring-2 focus:ring-[#551a8b]/40" type="button">Contact Us</button>
+
+                    <!-- btn:get storage  -->
+                    <button class="font inter font-medium text-sm px-5 py-2.5 rounded-lg text-[#551a8b] border border-[#551a8b]/60 transition-all duration-200 hover:text-white hover:bg-gradient-to-br hover:from-[#551a8b] hover:to-[#054f73] hover:shadow-[0_4px_14px_rgba(85,26,139,0.35)] focus:outline-none focus:ring-2 focus:ring-[#551a8b]/40" type="button">Get Storage</button>
+                </div>
             </div>
         </nav>
 
